44485138a279e25136940a39672f74c79d98ff5c
Amith Fernando <amith@fortunaglobal.com>
2017-05-02 11:34:05 +0530

    updated rest easy info

--- swagger-resteasy/src/main/java/com/amithfernando/swagger/resteasy/api/config/swagger/SwaggerBootstrap.java
+++ swagger-resteasy/src/main/java/com/amithfernando/swagger/resteasy/api/config/swagger/SwaggerBootstrap.java
@@ -13,7 +13,8 @@ public class SwaggerBootstrap extends HttpServlet {
         super.init(config);
 
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.2");
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setTitle("Test API with rest easy");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
         beanConfig.setBasePath("/api");
